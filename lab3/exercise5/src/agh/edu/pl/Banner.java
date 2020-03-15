1a3bb36be1284da4778c8bdcca9079aced12cc31
kamil <kamilskomrokamil@gmail.com>
2020-03-15 14:17:22 +0100

    lab3 ex5 add common form inputs

---
+++ lab3/exercise5/src/agh/edu/pl/Banner.java
@@ -0,0 +1,19 @@
+package agh.edu.pl;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+@ManagedBean(name = "banner")
+@ApplicationScoped
+public class Banner implements Serializable {
+
+    private int clicks = 0;
+
+    public int getClicks() {
+        return clicks;
+    }
+
+    public void incrementClicks() {
+        ++this.clicks;
+    }
+}
