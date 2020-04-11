3cca14df92ebd6f8b07b4e747c00d3baaa616dd9
kamil <kamilskomrokamil@gmail.com>
2020-04-11 19:06:15 +0200

    lab5 add init sql

--- lab5/book_manager/src/main/resources/META-INF/books.sql
+++ lab5/book_manager/src/main/resources/META-INF/books.sql
@@ -1,5 +1,1 @@
-INSERT INTO soa.books (author_surname, author_name, title, isbn, create_year, price) VALUES
-('Flanagan', 'John', 'The Ruins of Gorlan', '1231241252', 2015, 20.99),
-('Kyle', 'Chris', 'American sniper', '2823413421', 2012, 30.99),
-('Ferguson', 'Craig', 'American on purpose', '8172841281', 2014, 34.99),
-('Hurley', 'Graham', 'Raid 42', '3248198324', 1970, 22.50);
+INSERT INTO books (id, author_surname, author_name, title, isbn, create_year, price) VALUES (1,'Flanagan', 'John', 'The Ruins of Gorlan', '1231241252', 2015, 20.99),(2,'Kyle', 'Chris', 'American sniper', '2823413421', 2012, 30.99),(3,'Ferguson', 'Craig', 'American on purpose', '8172841281', 2014, 34.99),(4,'Hurley','Graham', 'Raid 42', '3248198324', 1970, 22.50);
